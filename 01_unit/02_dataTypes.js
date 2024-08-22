be09242585cc7cf2ec96ac62011336df94bc8ef9
Its_Udit <139812417+itsUdit24@users.noreply.github.com>
2024-08-22 07:30:18 +0000

    data types in here.

---
+++ 01_unit/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // but code readability should be high
+
+console.log("UDIT")
+
+
+let name = "udit"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value  (example can be temperature app, where if the server is down or some issue is there if it returns 0 which is equivalent to a temperature so it should return null in that case.)
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
