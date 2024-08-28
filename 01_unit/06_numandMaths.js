e20aefe04ecf8b702410e2c194726646f51e46b0
Its_Udit <139812417+itsUdit24@users.noreply.github.com>
2024-08-28 13:57:59 +0000

    Maths dot js updated

--- 01_unit/06_numandMaths.js
+++ 01_unit/06_numandMaths.js
@@ -1,23 +1,28 @@
 
 const score = 400
-// console.log(score);
+// console.log(score);  // 400
 
 const balance = new Number(100)
-// console.log(balance);
+// console.log(balance);  // [Number: 100]
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+// console.log(balance.toString().length);  // will become string and length of string is 3
+// console.log(balance.toFixed(2));         //100.00
 
-const otherNumber = 123.8966
+const otherNumber = 123.8966                    //23.8966, for 3 will be 23.9
+                    // (3) -> 124
+// console.log(otherNumber.toPrecision(4)); //123.9
 
-// console.log(otherNumber.toPrecision(4));
+const num2 = 1123.8966
+console.log(num2.toPrecision(3)); //1.12e+3,  -> ye lo 3 precision value bakki exponential mein deta hu
 
 const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
+console.log(hundreds.toLocaleString('en-IN')); // IN, cuz bydefault it is set to U.S standard
+
+// ********     Number. (various functions) - was done in console.   ********  
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
-// console.log(Math);
+console.log(Math);
 // console.log(Math.abs(-4));
 // console.log(Math.round(4.6));
 // console.log(Math.ceil(4.2));
