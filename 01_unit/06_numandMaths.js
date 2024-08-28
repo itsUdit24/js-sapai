2803d2109fb99012c5d568b623800fd48ab09901
Its_Udit <139812417+itsUdit24@users.noreply.github.com>
2024-08-28 14:50:47 +0000

    math random functions discussed.

--- 01_unit/06_numandMaths.js
+++ 01_unit/06_numandMaths.js
@@ -22,11 +22,11 @@ console.log(hundreds.toLocaleString('en-IN')); // IN, cuz bydefault it is set to
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
-console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
+console.log(Math); // Object [Math] {}  ->> Math apne ap mein ek object hai jiske kahi saare properties hain
+// console.log(Math.abs(-4));           ->> 4 
+// console.log(Math.round(4.6));        ->> 5
+// console.log(Math.ceil(4.2));         ->> 5
+// console.log(Math.floor(4.9));        ->> 4 
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
